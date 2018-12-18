baec4c0f282c9c2151e36953725f2cf2a65f1917
John Ratcliff <john.w.ratcliff@biola.edu>
2018-12-18 18:57:50 +0000

    Add sandbox.  Fix weird indentation issues.

--- app/routes/installation.js
+++ app/routes/installation.js
@@ -1,6 +1,7 @@
 import Route from 'emblem-site/routes/mixins/docs-route';
 import Docs from 'emblem-site/docs/installation';
+import deIndentDocs from 'emblem-site/lib/de-indent-docs';
 
 export default class InstallationRoute extends Route {
-  docs = Docs
+  docs = deIndentDocs(Docs)
 }
